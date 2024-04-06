a59ce080b7472e9308bbc409079ebf0c903072de
Libin Lalu <libinlalu000@gmail.com>
2024-04-06 18:30:47 +0530

    changed the version and modified gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as f:
 
 setup(
     name="spotidex",
-    version="0.0.2",
+    version="0.0.3",
     long_description=description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
