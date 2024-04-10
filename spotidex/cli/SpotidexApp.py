a8d64aaf2a8b5289597c2ee8466ba35a39d98c46
Libin Lalu <libinlalu000@gmail.com>
2024-04-11 00:07:53 +0530

    fixed bug: ffmpeg downloading not visible

--- spotidex/cli/SpotidexApp.py
+++ spotidex/cli/SpotidexApp.py
@@ -1,7 +1,8 @@
 from textual.app import App
 from spotidex.cli.app_screens.SearchInterface import Search
 from spotidex.cli.app_screens.SettingsInterface import Settings
-from spotidex.cli.utils import Path
+from spotidex.cli.utils import get_downloads_folder
+from spotidex.src.utils import get_ffmpeg
 
 
 class SpotidexApp(App):
@@ -12,13 +13,12 @@ class SpotidexApp(App):
     def on_ready(self) -> None:
         self.app.saved_settings = {
             "add_metadata": True,
-            "download_path": str(Path.cwd()),
+            "download_path": str(get_downloads_folder()),
             "download_quality": "high",
         }
         self.push_screen(Search())
- 
+
+
 def main():
+    get_ffmpeg()
     SpotidexApp().run()
-
-
-
